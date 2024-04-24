131ee19db806ebd9e1d94f98d115acfb60020647
Muhammad Ibrahim <160228904+Ibrahimnazee@users.noreply.github.com>
2024-04-24 19:24:58 +0500

    upadated bmi

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightinkg = 50;
+let heightinmeter = 4;
+let bmi = weightinkg / (heightinmeter * heightinmeter);
+console.log(`This is Your BMI ${bmi}`);
---
+++ index.ts
@@ -0,0 +1,7 @@
+
+
+let weightinkg = 50 ;
+let heightinmeter = 4 ;
+let bmi = weightinkg / (heightinmeter * heightinmeter);
+
+console.log(`This is Your BMI ${bmi}`);
